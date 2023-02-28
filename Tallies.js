1df90f23d8bbbfbad05dc2943a2119ac6388d8ab
Max Engel-Streich <maxees@gmail.com>
2023-02-28 15:53:09 -0500

    keywords!

--- Tallies.js
+++ Tallies.js
@@ -5,21 +5,8 @@ function Tallies ({ number, lineStyle, textColor, strikeThroughColor, textSize, 
   const [ones, setOnes] = useState('')
 
   useEffect(() => {
-    let i = 0
-    let newString5 = ''
-    while (i < Math.floor(number / 5)) {
-      newString5 += '|||| '
-      i++
-    }
-    setFives(newString5)
-
-    let j = 0
-    let newString1 = ''
-    while (j < number % 5) {
-      newString1 += '|'
-      j++
-    }
-    setOnes(newString1)
+    setFives(new Array(Math.floor(number / 5)).fill('|||| ').join(''))
+    setOnes(new Array(number % 5).fill('|').join(''))
   }, [number])
 
   return (
@@ -32,13 +19,4 @@ function Tallies ({ number, lineStyle, textColor, strikeThroughColor, textSize, 
   )
 }
 
-module.exports = Tallies
-
-Tallies.propTypes = {
-  number: Number,
-  lineStyle: String,
-  textColor: String,
-  strikeThroughColor: String,
-  textSize: String,
-  font: String
-}
+export default Tallies
