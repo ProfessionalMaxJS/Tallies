8728c8dda36af082c1707c4c3da2f16ac46d77ab
Max Engel-Streich <maxees@gmail.com>
2023-02-28 23:18:19 -0500

    from jsx to div

---
+++ src/components/Tallies.js
@@ -0,0 +1,20 @@
+import { React, useState, useEffect } from 'react'
+
+function Tallies ({ number, lineStyle, textColor, strikeThroughColor, textSize, font }) {
+  const [fives, setFives] = useState('')
+  const [ones, setOnes] = useState('')
+
+  useEffect(() => {
+    setFives(new Array(Math.floor(number / 5)).fill('|||| ').join(''))
+    setOnes(new Array(number % 5).fill('|').join(''))
+  }, [number])
+
+  return (
+          <div style={{ color: textColor, fontSize: textSize, fontFamily: font }}>
+                <span style={{ textDecorationLine: 'line-through', textDecorationStyle: lineStyle, textDecorationColor: strikeThroughColor }}>{fives}</span>
+                <span>{ones}</span>
+          </div>
+)
+}
+
+export default Tallies
